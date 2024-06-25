080c27e42bbacd8b10aac954a3bc4acedfbc19af
sumitumariya <sumitumariya11@gmail.com>
2024-06-26 00:14:17 +0530

    views.py

--- project/app/views.py
+++ project/app/views.py
@@ -4,15 +4,15 @@ from .serializers import *
 from .models import *
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 
-# class MovieViewSet(viewsets.ModelViewSet):
-# 	permission_classes = [IsAuthenticated]
-# 	queryset = MovieModel.objects.all()
-# 	serializer_class = MovieSerializer
+class MovieViewSet(viewsets.ModelViewSet):
+	permission_classes = [IsAuthenticated]
+	queryset = MovieModel.objects.all()
+	serializer_class = MovieSerializer
 
-# class StudentViewSet(viewsets.ModelViewSet):
-# 	permission_classes = [IsAdminUser]
-# 	queryset = StudentModel.objects.all()
-# 	serializer_class = StudentSerializer
+class StudentViewSet(viewsets.ModelViewSet):
+	permission_classes = [IsAdminUser]
+	queryset = StudentModel.objects.all()
+	serializer_class = StudentSerializer
 
 
 	
